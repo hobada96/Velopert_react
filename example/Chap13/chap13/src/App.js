0c8efd8be8612f9b460c58fd96a7307d38b80855
hobada96 <hobada96@gmail.com>
2021-03-03 11:12:17 +0900

    Add Chap15

--- example/Chap13/chap13/src/App.js
+++ example/Chap13/chap13/src/App.js
@@ -3,59 +3,79 @@ import UserList from './UserList';
 import CreateUser from './CreateUser';
 
 function App() {
-  const [inputs, setInputs] = useState({
-    username: '',
-    email: ''
-  });
-  const { username, email } = inputs;
-  const onChange = e => {
-    const { name, value } = e.target;
-    setInputs({
-      ...inputs,
-      [name]: value
+    const [inputs, setInputs] = useState({
+        username: '',
+        email: ''
     });
-  };
-
-  const [users,setUsers] = useState([
-    {
-      id: 1,
-      username: 'velopert',
-      email: 'public.velopert@gmail.com'
-    },
-    {
-      id: 2,
-      username: 'tester',
-      email: 'tester@example.com'
-    },
-    {
-      id: 3,
-      username: 'liz',
-      email: 'liz@example.com'
-    }
-  ]);
-
-  const nextId = useRef(4);
-  console.log(nextId)
-  const onCreate = () => {
-    const user = {
-      id : nextId.current,
-      username,
-      email
+    const { username, email } = inputs;
+    const onChange = e => {
+        const { name, value } = e.target;
+        setInputs({
+            ...inputs,
+            [name]: value
+        });
     };
-    setUsers([...users,user])
-    nextId.current += 1;
-  };
-  return (
-      <>
-        <CreateUser
-            username={username}
-            email={email}
-            onChange={onChange}
-            onCreate={onCreate}
-        />
-        <UserList users={users} />
-      </>
-  );
+    const [users, setUsers] = useState([
+        {
+            id: 1,
+            username: 'velopert',
+            email: 'public.velopert@gmail.com',
+            active: true
+        },
+        {
+            id: 2,
+            username: 'tester',
+            email: 'tester@example.com',
+            active: false
+        },
+        {
+            id: 3,
+            username: 'liz',
+            email: 'liz@example.com',
+            active: false
+        }
+    ]);
+
+    const nextId = useRef(4);
+    const onCreate = () => {
+        const user = {
+            id: nextId.current,
+            username,
+            email
+        };
+        setUsers(users.concat(user));
+
+        setInputs({
+            username: '',
+            email: ''
+        });
+        nextId.current += 1;
+    };
+
+    const onRemove = (id) => {
+        // user.id 가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
+        // = user.id 가 id 인 것을 제거함
+        setUsers(users.filter(user => user.id !== id));
+    };
+
+    const onToggle = (id) =>{
+        setUsers(
+            users.map(
+                user => user.id === id ? {...user, active: !user.active}:user
+            )
+        )
+    }
+    return (
+        <>
+            <CreateUser
+                username={username}
+                email={email}
+                onChange={onChange}
+                onCreate={onCreate}
+            />
+            <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
+        </>
+    );
 }
 
 export default App;
