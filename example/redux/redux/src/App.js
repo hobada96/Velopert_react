808b0db6c504335914709446b90e39bdb2e3d070
hobada96 <hobada96@gmail.com>
2021-03-04 11:22:30 +0900

    Chap 17

--- example/redux/redux/src/App.js
+++ example/redux/redux/src/App.js
@@ -1,25 +1,8 @@
-import './App.css';
-import { createStore} from 'redux';
-const initalState = {
-  counter: 0,
-  text : '',
-  list : []
-};
-
-const INCREASE = 'INCREASE';
-const DECREASE = 'DECREASE';
-const CHANGE_TEXT = 'CHANGE_TEXT';
-const ADD_TO_LIST = 'ADD_TO_LIST';
-
-function increase(){
-  return {
-    type: INCREASE
-  }
-}
-
 function App() {
   return (
-
+      <div>
+        <h1>Hello</h1>
+      </div>
   );
 }
 
