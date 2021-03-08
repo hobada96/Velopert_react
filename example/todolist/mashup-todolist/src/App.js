bf13f0757fc8b17fbb14a2c020baf75eb1df439b
hobada96 <hobada96@gmail.com>
2021-03-08 19:26:37 +0900

    add UI

--- example/todolist/mashup-todolist/src/App.js
+++ example/todolist/mashup-todolist/src/App.js
@@ -1,8 +1,25 @@
+import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+import TodoTemplate from "./components/TodoTemplate";
+import TodoHead from "./components/TodoHead"
+import TodoList from "./components/TodoList";
+import TodoCreate from "./components/TodoCreate";
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: #e9ecef;
+  }
+`;
+
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <>
+      <GlobalStyle/>
+      <TodoTemplate>
+        <TodoHead/>
+        <TodoList/>
+        <TodoCreate/>
+      </TodoTemplate>
+    </>
   );
 }
 
