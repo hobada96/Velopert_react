e4196d2e811ec063d4f903a87a7f0df6d17396a8
hobada96 <hobada96@gmail.com>
2021-03-08 11:32:09 +0900

    Chap22 use Context api

--- example/Chap21/chap21/src/App.js
+++ example/Chap21/chap21/src/App.js
@@ -54,6 +54,8 @@ function reducer(state, action) {
     }
 }
 
+export const UserDispatch = React.createContext(null);
+
 function App() {
     const [{username, email}, onChange, reset] = useInputs({
         username: '',
@@ -61,8 +63,8 @@ function App() {
     });
     const [state, dispatch] = useReducer(reducer, initialState);
     const nextId = useRef(4);
-
     const {users} = state;
+
     const onCreate = useCallback(() => {
         dispatch({
             type: 'CREATE_USER',
@@ -76,32 +78,19 @@ function App() {
         nextId.current += 1;
     }, [username, email, reset]);
 
-    const onToggle = useCallback(id => {
-        dispatch({
-            type: 'TOGGLE_USER',
-            id
-        });
-    }, [])
-
-    const onRemove = useCallback(id => {
-        dispatch({
-            type: 'REMOVE_USER',
-            id
-        });
-    }, []);
 
     const count = useMemo(() => countActiveUsers(users), [users]);
     return (
-        <>
+        <UserDispatch.Provider value={dispatch}>
             <CreateUser
                 username={username}
                 email={email}
                 onChange={onChange}
                 onCreate={onCreate}
             />
-            <UserList users={users} onToggle={onToggle} onRemove={onRemove} />
+            <UserList users={users}/>
             <div>활성 사용자수 : {count}</div>
-        </>
+        </UserDispatch.Provider>
     );
 }
 
