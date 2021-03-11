bfc0618934bcc436ba1f9645b8080faf5008e46e
hobada96 <hobada96@gmail.com>
2021-03-11 10:29:47 +0900

    add exercise case

--- example/redux/redux/src/index.js
+++ example/redux/redux/src/index.js
@@ -1,15 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { Provider } from 'react-redux';
-import {createStore} from "redux";
+import './exercise'
 
-const store = createStore(rootReducer)
-ReactDOM.render(
-  <React.StrictMode>
-      <Provider store={store}>
-        <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
